6d78c6a730a793698baf2f7fd911615f383394bf
kannan <kannan.90@gmail.com>
2015-10-27 21:55:01 +0800

    Homework 1

--- MOB-Test-Assignment/MOB-Test-Assignment/ViewController.swift
+++ MOB-Test-Assignment/MOB-Test-Assignment/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepper: UIStepper!
+    @IBAction func stepperPressed(sender: UIStepper) {
+        print(stepper.value)
+        var a = String(stepper.value)
+        print(a + "hello")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
